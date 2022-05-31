96e9259737519131742aa9e14436d2faff96ea36
amine-deboosere <amine.deboosere@ntico.com>
2022-05-31 16:50:09 +0200

    first commit

---
+++ src/testSonarCloud/test1.java
@@ -0,0 +1,20 @@
+package testSonarCloud;
+
+public class test1 {
+	public int i;
+	public String s;
+	public boolean b;
+	
+	public test1() {
+		this.i = 0;
+		this.s = "";
+		this.b = false;
+	}
+	public test1(int ii, String ss, boolean bb) {
+		this.i = ii;
+		this.s = ss;
+		this.b = bb;
+	}
+	
+	
+}
